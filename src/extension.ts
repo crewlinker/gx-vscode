de80e2a99b1adb71408a4f413a6d1b714f6f82c1
Ad van der Veer <advanderveer@gmail.com>
2023-06-06 15:32:22 +0200

    empty extension

---
+++ src/extension.ts
@@ -0,0 +1,11 @@
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate() {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('"gx" extension is now active!');
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
