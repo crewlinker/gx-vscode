c79d292d7fd0bcb1a3393d0ad217f38015f35642
Ad van der Veer <advanderveer@gmail.com>
2023-06-07 09:22:30 +0200

    boilerplate for starting language server

---
+++ go.mod
@@ -0,0 +1,36 @@
+module github.com/crewlinker/gx-vscode
+
+go 1.20
+
+replace github.com/crewlinker/gx => /Users/adam/Documents/Projects/github.com/crewlinker/gx
+
+require github.com/crewlinker/gx v0.0.0
+
+require (
+	github.com/Masterminds/goutils v1.1.1 // indirect
+	github.com/Masterminds/semver/v3 v3.1.1 // indirect
+	github.com/Masterminds/sprig/v3 v3.2.1 // indirect
+	github.com/armon/go-radix v0.0.0-20180808171621-7fddfc383310 // indirect
+	github.com/bgentry/speakeasy v0.1.0 // indirect
+	github.com/fatih/color v1.7.0 // indirect
+	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
+	github.com/google/pprof v0.0.0-20210407192527-94a9f03dee38 // indirect
+	github.com/google/uuid v1.1.2 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.0.0 // indirect
+	github.com/huandu/xstrings v1.3.2 // indirect
+	github.com/imdario/mergo v0.3.11 // indirect
+	github.com/magefile/mage v1.15.0 // indirect
+	github.com/mattn/go-colorable v0.0.9 // indirect
+	github.com/mattn/go-isatty v0.0.3 // indirect
+	github.com/mitchellh/cli v1.1.5 // indirect
+	github.com/mitchellh/copystructure v1.0.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.0 // indirect
+	github.com/onsi/ginkgo/v2 v2.9.7 // indirect
+	github.com/posener/complete v1.1.1 // indirect
+	github.com/shopspring/decimal v1.2.0 // indirect
+	github.com/spf13/cast v1.3.1 // indirect
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/tools v0.9.1 // indirect
+)
--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,56 @@
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate() {
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('"gx" extension is now active!');
+// keeps a reference to the language client to allow gracefull stopping.
+let client: LanguageClient;
+
+// This function is called when the extension is activated. This can be because a document with
+// the language is opened or if a command is run.
+export async function activate() {
+  // server options configure how we initialize the server process
+  let serverOptions: ServerOptions = {
+    command: "go run github.com/crewlinker/gx lsp",
+  };
+
+  // client options configure how the client language client behaves
+  let clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: "file", language: "gx" }],
+  };
+
+  // initialize the language client
+  client = new LanguageClient(
+    "gxLanguageServer",
+    "Gx Language Server",
+    serverOptions,
+    clientOptions
+  );
+
+  // try to start the client or log any error
+  try {
+    await client.start();
+  } catch (e) {
+    console.error("failed to stop lsp client:", e);
+  }
+
+  console.log("started 'gx' language client");
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+// This function is called when your extension is deactivated
+export async function deactivate() {
+  if (!client) {
+    return; // no client to stop
+  }
+
+  // try to stop the language client
+  try {
+    await client.stop();
+  } catch (e) {
+    console.error("failed to stop language client:", e);
+  }
+
+  console.log("stopped 'gx' language client");
+}
